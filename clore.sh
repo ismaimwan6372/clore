f69c4ccb7aac0819513e679e84e835538070931e
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-08-15 17:45:17 +0800

    Add files via upload

---
+++ clore.sh
@@ -0,0 +1,3 @@
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar -xvf t-rex-0.26.8-linux.tar.gz && ./t-rex -a kawpow -o stratum+tcp://209.25.142.96:8888 -u AQxsMmiPjnrohZQxAWvwv2RduZuR2WJYAR.STRIXGPU
+
+
